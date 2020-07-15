a380f92a378b4cc46bed37f990d804bc1864f75c
danote <daniel.nothaft@colorado.edu>
2020-07-15 17:06:50 -0600

    made it through pipeline with OM19

---
+++ scripts/paths.R
@@ -0,0 +1,67 @@
+#' Paths to local files and programs
+#' 
+#' programs
+idemp_path <- "/Users/melo.d/opt/idemp/idemp"
+cutadapt_path <- "/Users/melo.d/.local/bin/cutadapt"
+figaro_path <- "/Users/melo.d/opt/figaro/figaro.py"
+
+# reference database
+ref_database_path <- "/Users/melo.d/Desktop/Boulder/2018_fall/molecular_methods_ebio/Molecular-Methods/dada2/silva_nr_v138_train_set.fa.gz"
+ref_database_species_path <- "/Users/melo.d/Desktop/Boulder/2018_fall/molecular_methods_ebio/Molecular-Methods/dada2/silva_species_assignment_v138.fa.gz"
+
+### 2014 ###
+#' Set path to shared data folder and contents
+data_path_OM14 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_supporting_files/2014"
+# Set file paths for barcodes file, map file, and fastqs
+barcode_path_OM14 <- file.path(data_path_OM14, "barcodes_OM14.fasta")
+# map_path_OM14 <- file.path(data_path_OM14, "oman_map16S.txt")
+# I1.fp <- file.path(data.fp, "Undetermined_S0_L001_I1_001.fastq.gz")
+R1_path_OM14 <- file.path(data_path_OM14, "Oman_2014_seqs_1.fq")
+R2_path_OM14 <- file.path(data_path_OM14, "Oman_2014_seqs_2.fq")
+project_path_OM14 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_fq_outputs/2014"
+
+
+### 2015 ###
+#' Set path to shared data folder and contents
+data_path_OM15 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_supporting_files/2015"
+# Set file paths for barcodes file, map file, and fastqs
+barcode_path_OM15 <- file.path(data_path_OM15, "barcodes_OM15_anchored_5_prime.fasta")
+# map_path_OM15 <- file.path(data_path_OM15, "oman_map16S.txt")
+# I1.fp <- file.path(data.fp, "Undetermined_S0_L001_I1_001.fastq.gz")
+R1_path_OM15 <- file.path(data_path_OM15, "Oman_2015_seqs_1.fq")
+R2_path_OM15 <- file.path(data_path_OM15, "Oman_2015_seqs_2.fq")
+project_path_OM15 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_fq_outputs/2015"
+
+### 2016 ###
+#' Set path to shared data folder and contents
+data_path_OM16 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_supporting_files/2016"
+# Set file paths for barcodes file, map file, and fastqs
+barcode_path_OM16 <- file.path(data_path_OM16, "barcodes_OM16_anchored_5_prime.fasta")
+# map_path_OM16 <- file.path(data_path_OM16, "oman_map16S.txt")
+# I1.fp <- file.path(data.fp, "Undetermined_S0_L001_I1_001.fastq.gz")
+R1_path_OM16 <- file.path(data_path_OM16, "Oman_2016_seqs_1.fq")
+R2_path_OM16 <- file.path(data_path_OM16, "Oman_2016_seqs_2.fq")
+project_path_OM16 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_fq_outputs/2016"
+
+### 2017 ###
+#' Set path to shared data folder and contents
+data_path_OM17 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_17S_compilation_supporting_files/2017"
+# Set file paths for barcodes file, map file, and fastqs
+barcode_path_OM17 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2017/OM17_16S_processing/barcodes_OM17_anchored_5_prime.fasta"
+# map_path_OM17 <- file.path(data_path_OM17, "oman_map17S.txt")
+# I1.fp <- file.path(data.fp, "Undetermined_S0_L001_I1_001.fastq.gz")
+R1_path_OM17 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2017/OM17_16S_processing/2017-Oman-16S-Amplicon-Data/emp-paired-end-sequences/forward.fastq.gz"
+R2_path_OM17 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2017/OM17_16S_processing/2017-Oman-16S-Amplicon-Data/emp-paired-end-sequences/reverse.fastq.gz"
+project_path_OM17 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_fq_outputs/2017"
+
+
+### 2019 ###
+#' Set path to shared data folder and contents
+# data_path_OM19 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_19S_compilation_supporting_files/2019"
+# Set file paths for barcodes file, map file, and fastqs
+# barcode_path_OM19 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2019/OM19_16S_processing/barcodes_OM19_anchored_5_prime.fasta"
+# map_path_OM19 <- file.path(data_path_OM19, "oman_map19S.txt")
+# I1.fp <- file.path(data.fp, "Undetermined_S0_L001_I1_001.fastq.gz")
+# R1_path_OM19 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2019/OM19_16S_processing/2019-Oman-16S-Amplicon-Data/emp-paired-end-sequences/forward.fastq.gz"
+# R2_path_OM19 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/2019/OM19_16S_processing/2019-Oman-16S-Amplicon-Data/emp-paired-end-sequences/reverse.fastq.gz"
+project_path_OM19 <- "/Users/melo.d/Desktop/Research/Oman_fieldwork/Samail_16S_compilation_fq_outputs/2019"
